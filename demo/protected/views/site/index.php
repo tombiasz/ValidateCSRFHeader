28fb987a8ba8f60f58df28129379dd3e2f40fa60
tombiasz <tomasz.m.nowacki@gmail.com>
2016-12-22 17:49:32 +0100

    dispaly csrf and cookie validation config

--- demo/protected/views/site/index.php
+++ demo/protected/views/site/index.php
@@ -5,5 +5,19 @@
     </head>
     <body>
         <h1>ValidateCSRFHeaderFilter Demo</h1>
+
+        <h2>Config</h2>
+        <p>
+            <strong>CSRF protection enabled:</strong>
+            <?php
+                echo Yii::app()->request->enableCsrfValidation ? 'true' : 'false';
+            ?>
+        </p>
+        <p>
+            <strong>Cookie validation enabled:</strong>
+            <?php
+                echo Yii::app()->request->enableCookieValidation ? 'true' : 'false';
+            ?>
+        </p>
     </body>
 </html>
