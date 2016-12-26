f416387f6f17cffcf019c18bb9146a4a2cfd51c2
tombiasz <tomasz.m.nowacki@gmail.com>
2016-12-26 18:39:56 +0100

    force csrf token cookie creation in index action

--- demo/protected/controllers/SiteController.php
+++ demo/protected/controllers/SiteController.php
@@ -3,15 +3,12 @@
 class SiteController extends Controller
 {
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
-	}
+    public function actionIndex()
+    {
+        // force CSRF cookie
+        Yii::app()->request->getCsrfToken();
+
+        $this->render('index');
+    }
 
 }
